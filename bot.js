2a9efd9aade1a64301ce070c1b6bae991e18e31e
belalnoorzai <47641716+belalnoorzai@users.noreply.github.com>
2019-02-16 17:55:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["424134723716120578"]
+const developers = ["535917566330404864"]
 const adminprefix = "^";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
